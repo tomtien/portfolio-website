ff3d0dd3fb2334c6df5ea7bbd3a5703b2d3f9352
GamingBox <64324389+tomtien@users.noreply.github.com>
2024-06-02 18:35:03 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
                             />
                             <div className=" p-4 max-w-7xl  mx-auto relative  w-full ">
                                 <h1 className="text-6xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-                                    Jeroen
+                                    GamingBox
                                 </h1>
                                 <h2 className="text-2xl md:text-4xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-blue-50 to-blue-400 bg-opacity-50">
                                     Full Stack Developer
